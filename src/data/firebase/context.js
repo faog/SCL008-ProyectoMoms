d8ebb4b61bbce6622b0523c6a7bf6ac5339d5745
faog <fabiola.orellana.g@gmail.com>
2019-07-21 16:58:03 -0400

    configuración de archivos para funcionamiento de firebase

---
+++ src/data/firebase/context.js
@@ -0,0 +1,5 @@
+import React from 'react';
+// Declara el contexto de firebase, que permite que firebase exista en toda la aplicación
+const FirebaseContext = React.createContext(null);
+
+export default FirebaseContext;
---
+++ src/data/firebase/index.js
@@ -0,0 +1,6 @@
+import FirebaseContext from './context';
+import Firebase from './firebase';
+// Sirve para agrupar todas las funciones de firebase en un solo lugar
+export default Firebase;
+
+export { FirebaseContext };
