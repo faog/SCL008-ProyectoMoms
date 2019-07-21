0d41232f71b678ab95c475f50c1cbabff7147442
faog <fabiola.orellana.g@gmail.com>
2019-07-21 17:08:07 -0400

    Configuración eslint y corrección de archivos

--- src/data/firebase/firebase.js
+++ src/data/firebase/firebase.js
@@ -1,15 +1,16 @@
+/* eslint-disable import/no-unresolved */
 import app from 'firebase/app';
 import 'firebase/firestore';
 
 // Inicializacion del proyecto en firebase
 const config = {
-  apiKey: "AIzaSyB7lktfPzNHohAT7O_ZKjcXa1gwjGZsmEQ",
-  authDomain: "proyecto-moms.firebaseapp.com",
-  databaseURL: "https://proyecto-moms.firebaseio.com",
-  projectId: "proyecto-moms",
-  storageBucket: "",
-  messagingSenderId: "1052432304334",
-  appId: "1:1052432304334:web:b1be1179280dc3ad"
+  apiKey: 'AIzaSyB7lktfPzNHohAT7O_ZKjcXa1gwjGZsmEQ',
+  authDomain: 'proyecto-moms.firebaseapp.com',
+  databaseURL: 'https://proyecto-moms.firebaseio.com',
+  projectId: 'proyecto-moms',
+  storageBucket: '',
+  messagingSenderId: '1052432304334',
+  appId: '1:1052432304334:web:b1be1179280dc3ad',
 };
 
 // Crear clase Firebase, que permite guardar la información de las empresas
