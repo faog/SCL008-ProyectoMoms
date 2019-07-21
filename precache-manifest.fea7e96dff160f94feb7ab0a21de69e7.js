0a1bbf7aacb684c81cb835e50cdc32fc0d988535
faog <fabiola.orellana.g@gmail.com>
2019-07-21 16:00:28 -0400

    Updates

---
+++ precache-manifest.fea7e96dff160f94feb7ab0a21de69e7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20032bd13a6dc6a6e8666a328ded7282",
+    "url": "/SCL008-ProyectoMoms/index.html"
+  },
+  {
+    "revision": "3d0af5b019b08673d328",
+    "url": "/SCL008-ProyectoMoms/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/SCL008-ProyectoMoms/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "3d0af5b019b08673d328",
+    "url": "/SCL008-ProyectoMoms/static/js/main.9323bd74.chunk.js"
+  },
+  {
+    "revision": "4b18b4c539ce40148821",
+    "url": "/SCL008-ProyectoMoms/static/js/runtime~main.09163e2e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/SCL008-ProyectoMoms/static/media/logo.5d5d9eef.svg"
+  }
+]);
