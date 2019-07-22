c942f9171a7c8712eba48c177b4bcdd0d568f045
faog <fabiola.orellana.g@gmail.com>
2019-07-22 10:21:16 -0400

    Prueba de rutas

--- src/App.js
+++ src/App.js
@@ -12,15 +12,15 @@ function App() {
     <Router>
       <header>
         <ul>
-          <li><NavLink to="/" exact activeClassName="active">Home</NavLink></li>
+          <li><NavLink to="/SCL008-ProyectoMoms/" exact activeClassName="active">Home</NavLink></li>
           <li><NavLink to="/about" exact activeClassName="active">About</NavLink></li>
         </ul>
       </header>
       <main>
         <Switch>
-          <Route path="/" exact component={templateHome} />
-          <Route path="/about" component={templateAbout} />
-          <Route path="/formulario" component={templateForm} />
+          <Route path="/SCL008-ProyectoMoms/" exact component={templateHome} />
+          <Route path="/SCL008-ProyectoMoms/about" component={templateAbout} />
+          <Route path="/SCL008-ProyectoMoms/formulario" component={templateForm} />
         </Switch>
       </main>
     </Router>
