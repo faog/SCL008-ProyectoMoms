5dc770c1a4d604b939cda87a053b6100dfbdeff3
faog <fabiola.orellana.g@gmail.com>
2019-07-21 21:51:14 -0400

    Creación de templates de prueba

---
+++ src/templates/templateForm.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class templateForm extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <h1>Formulario</h1>
+      </React.Fragment>
+    );
+  }
+}
+
+export default templateForm;
