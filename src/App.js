ca7b583f83130b853676601347b00d565d96e5dc
faog <fabiola.orellana.g@gmail.com>
2019-07-21 21:52:00 -0400

    Configuración de router para los template creados y css básico

--- src/App.js
+++ src/App.js
@@ -1,29 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
+import {
+  BrowserRouter as Router, Route, Switch, NavLink,
+} from 'react-router-dom';
+import templateHome from './templates/templateHome';
+import templateAbout from './templates/templateAbout';
+import templateForm from './templates/templateForm';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          {' '}
-          <code>src/App.js</code>
-          {' '}
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <Router>
+      <header>
+        <ul>
+          <li><NavLink to="/" exact activeClassName="active">Home</NavLink></li>
+          <li><NavLink to="/about" exact activeClassName="active">About</NavLink></li>
+        </ul>
       </header>
-    </div>
+      <main>
+        <Switch>
+          <Route path="/" exact component={templateHome} />
+          <Route path="/about" component={templateAbout} />
+          <Route path="/formulario" component={templateForm} />
+        </Switch>
+      </main>
+    </Router>
   );
 }
 
