0161849dd890ef61e5bef4f419cfa5c8962a2cc3
TaphieM <taffymayen@gmail.com>
2019-07-23 14:02:10 -0500

    se agrega menu de hamburguesa y flecha de conocer mas

--- src/templates/templateHome.js
+++ src/templates/templateHome.js
@@ -1,22 +1,25 @@
 import React, { Component } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-// import { faArrowCircleDown } from '@fortawesome/free-solid-svg-icons';
+import { faArrowAltCircleDown } from '@fortawesome/free-solid-svg-icons';
+import ComponentVisualButton from '../components/componentVisualButton';
 import './css/templateHome.css';
 
 class templateHome extends Component {
   render() {
     return (
       <React.Fragment>
-        <div className="happyTalk">
-          <h4>Mas de mil mujeres y compañías nos avalan</h4>
-          <h1>Conectamos talento femenino con las mejores compañías que tienen ofertas flexibles para ellas</h1>
-        </div>
-        <div className="call2Action">
-          <button className="join">Únete</button>
-          <button className="enter">Ingresa</button>
-          <span>Conoce más</span>
-          {/* <FontAwesomeIcon icon="faArrowCircleDown" /> */}
-        </div>
+        <article className="firstPage">
+          <div className="happyTalk">
+            <h4>Mas de mil mujeres y compañías nos avalan</h4>
+            <h1>Conectamos talento femenino con las mejores compañías que tienen ofertas flexibles para ellas</h1>
+          </div>
+          <div className="call2Action">
+            <ComponentVisualButton name="Únete" className="join"  />
+            <ComponentVisualButton name="Ingresa" className="enter" />
+            <span>Conoce más</span>
+            <FontAwesomeIcon className="arrowDown" icon={faArrowAltCircleDown} />
+          </div>
+        </article>
       </React.Fragment>
     );
   }
