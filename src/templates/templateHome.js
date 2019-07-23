adb6d620b5eb0008464406d116e4f5f2e27612a9
TaphieM <taffymayen@gmail.com>
2019-07-22 22:29:55 -0500

    prototipo de página inicial, falta flecha de ver mas y menu hamburguesa

--- src/templates/templateHome.js
+++ src/templates/templateHome.js
@@ -1,10 +1,19 @@
 import React, { Component } from 'react';
+import './templateHome.css';
 
 class templateHome extends Component {
   render() {
     return (
       <React.Fragment>
-        <h1>Home</h1>
+        <div className="happyTalk">
+        <h4>Mas de mil mujeres y compañías nos avalan</h4>
+        <h1>Conectamos talento femenino con las mejores compañías que tienen ofertas flexibles para ellas</h1>
+        </div>
+        <div className="call2Action">
+        <button className="join">Únete</button>
+        <button className="enter">Ingresa</button>
+        <span>Conoce más</span>
+        </div>
       </React.Fragment>
     );
   }
