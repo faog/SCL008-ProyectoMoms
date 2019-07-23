5ccbf6679aef040dbc309ba7dc0ca4c4d39aefc5
faog <fabiola.orellana.g@gmail.com>
2019-07-22 21:55:44 -0400

    Actualización del template, leyendo la información desde el JSON; además de agregar el contexto de firebase

--- src/templates/templateForm.js
+++ src/templates/templateForm.js
@@ -1,20 +1,30 @@
 import React, { Component } from 'react';
 import ComponentVisualSelect from '../components/componentVisualSelect';
 import ComponentVisualInput from '../components/componentVisualInput';
+import ComponentVisualTextArea from '../components/componentVisualTextArea';
+import { FirebaseContext } from '../data/firebase';
 
+const selectFile = require('../data/informationSelect');
 
 class templateForm extends Component {
   render() {
     return (
-      <React.Fragment>
-        <h1>Formulario</h1>
-        <ComponentVisualSelect message="Flexibilidad: " />
-        <ComponentVisualSelect message="Carrera: " />
-        <ComponentVisualSelect message="Idioma : " />
-        <ComponentVisualSelect message="Años de experiencia : " />
-        <ComponentVisualSelect message="Rango de sueldo estimado :" />
-        <ComponentVisualInput message="Nombre del cargo: " validate={this.validateName} />
-      </React.Fragment>
+      <FirebaseContext.Consumer>
+        <React.Fragment>
+          <h1>Formulario</h1>
+          <ComponentVisualInput message="Nombre del cargo " validate={this.validateName} />
+          <ComponentVisualSelect message="Flexibilidad otorgada " options={selectFile.flexibilidad} />
+          <ComponentVisualSelect message="Idioma " options={selectFile.idioma} />
+          <ComponentVisualSelect message="Nivel " options={selectFile.nivel} />
+          <ComponentVisualSelect message="Carrera " options={selectFile.carrera} />
+          <ComponentVisualSelect message="Nivel de estudios " options={selectFile.nivelEstudios} />
+          <ComponentVisualSelect message="Años de experiencia " options={selectFile.experiencia} />
+          <ComponentVisualSelect message="Rango de sueldo estimado " options={selectFile.rangoSueldo} />
+          <ComponentVisualTextArea message="Descripción del cargo" />
+          <ComponentVisualTextArea message="Tareas claves:" />
+          <ComponentVisualTextArea message="Educación adicional:" />
+        </React.Fragment>
+      </FirebaseContext.Consumer>
     );
   }
 }
