0b1fafae3711671e6af285d125648dbe3c638093
TaphieM <taffymayen@gmail.com>
2019-07-24 16:35:37 -0500

    modal con css listo pero problemas con estado

--- src/components/componentVisualModal.js
+++ src/components/componentVisualModal.js
@@ -1,22 +1,24 @@
 import React from 'react';
 import './css/componentVisualModal.css';
 
-const componentVisualModal = (props) => {
-  if (!props.onOpen) {
+
+const ComponentVisualModal = (props) => {
+ if (!props.isOpen) {
     return null;
   }
-  return (
+  return( 
     <div className="modal">
-      <div className="text-container">
-        <span className="confirmation">{props.confirmation}</span>
+    <div className="modal-container">
+    <div className="text-container">
+        <p className="confirmation">{props.confirmation}</p>
         <h2 className="message">{props.message}</h2>
-        <hr className="line" />
-        <p className="text">{props.text}</p>
-        <button className="close-btn" onClick={props.onClose}><FontAwesomeIcon className="cross" icon={faTimes}/>Entendido</button>
-      </div>
+         <p className="text">{props.text}</p>
+         <button className="close-btn" onClick={props.onClose}>Entendido</button>
+       </div>
+       </div>  
     </div>
   );
 };
 
 
-export default componentVisualModal;
+export default ComponentVisualModal;
